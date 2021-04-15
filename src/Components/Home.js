83ca56dbf7724d191163904bd5688cc1d613ce5f
Pranathi-07 <60241219+Pranathi-07@users.noreply.github.com>
2021-04-15 12:26:37 -0500

    UI changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,24 +4,79 @@ import './Home.css'
 
 
 
-
-const Home = () => {
+class Home extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            showCatogeries:false,
+            showOffers:false,
+            showBrands:false
+        }
+        
+            
+        
+    }
+    render(){
     return (
         <>
             <div className='container-page'>
                 <div className='grid-item1'>
-                   <div className="menu-items">
+                   <div className="menu-items ">
                        <text className="menu-text">Catogeries</text>
-                       <i className="fas fa-plus"></i>
+                       <div>
+                            
+                       <a href='#'><i className="fas fa-plus" onClick={()=>{this.setState({showCatogeries:!this.state.showCatogeries})}}></i></a>
+                        </div>
                     </div>
+                        
+                            {
+                                this.state.showCatogeries? <div className='submenu'>
+                                    <a><div>Electronics</div></a>
+                                    <a><div>groceries</div></a>
+                                    <a><div>Clothing</div></a>
+                                    <a><div>Jwelery</div></a>
+                                    </div> : null
+                                }
+                        
+                        
+                    
                     <div className="menu-items">
                        <text className="menu-text">Offers</text>
-                       <i className="fas fa-plus"></i>
-                    </div>
+                       <div>
+                            
+                       <a href='#'><i className="fas fa-plus" onClick={()=>{this.setState({showOffers:!this.state.showOffers})}} ></i></a>
+                        </div>
+                        </div>
+                            {
+                                this.state.showOffers? <div className='submenu'>
+                                    <a><div>Electronics</div></a>
+                                    <a><div>groceries</div></a>
+                                    <a><div>Clothing</div></a>
+                                    <a><div>Jwelery</div></a>
+                                    </div> : null
+                                }
+                        
+                        
+                    
                     <div className="menu-items">
                        <text className="menu-text">Brands</text>
-                       <i className="fas fa-plus"></i>
-                    </div>  
+                            <div>
+                                
+                                <a href='#'><i className="fas fa-plus" onClick={()=>{this.setState({showBrands:!this.state.showBrands})}} ></i></a>
+                        
+                            </div>
+                            </div>
+                        
+                            {
+                                this.state.showBrands? <div className='submenu'>    
+                                    <div><a href='https://www.amazon.com/'>Amazon</a></div>
+                                    <div><a href='https://www.walmart.com/'>Walmart</a></div>
+                                    <div><a href='https://www.costco.com/'>Costco</a></div>
+                                    <div><a href='https://www.bestbuy.com/'>Best Buy</a></div>
+                                    </div> : null
+                            }
+                        
+                      
 
     
                 </div>
@@ -31,16 +86,39 @@ const Home = () => {
                         <h2>Search for your products and compare prices</h2>
                         <div className="input">
                             <input type="search" id="form1" className="form-control" placeholder='Search or enter the products' />
-                            <button type="button" className="btn btn-primary">
+                            <button type="button"  className="btn btn-primary">
                                 <i className="fas fa-search"></i>
                             </button>
                         </div>
                     </div>
-    
+                    <div className='card'>
+                        <div className='card-item'>
+                            <img src="https://i.pinimg.com/originals/08/5f/d8/085fd8f7819dee3b716da73d3b2de61c.jpg"></img>
+                            <div className='details'>
+                                <h4><b>Amazon</b></h4>
+                                
+                            </div>
+                            </div>
+                            <div className='card-item'>
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Walmart_Spark.svg/722px-Walmart_Spark.svg.png"></img>
+                            <div className='details'>
+                                <h4><b>Walmart</b></h4>
+                                
+                            </div>
+                            </div>
+                            <div className='card-item'>
+                            <img src="https://corporate.aldi.us/fileadmin/fm-dam/logos/ALDI_2017.png"></img>
+                            <div className='details'>
+                                <h4><b>Aldi</b></h4>
+                                
+                            </div>
+                            </div>
+                    </div> 
                 </div>
             </div>
         </>
     );
+}
 }
 
 export default Home;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,23 +5,23 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Navbar = ( ) => {
     return(<>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
                     <li>
                         <img src="https://media.istockphoto.com/vectors/yellow-lines-geometric-vector-logo-letter-c-vector-id1171091258?k=6&m=1171091258&s=612x612&w=0&h=VQ3FNuAsABNoNajTiYMrgc4ahbdUn7sz1zhr3VvkqY4="></img>
                     </li>
-                    <li class="nav-item active"><a class="nav-link" href="/">ChooseVise</a></li>
-                    <li style={{float: 'right'}} class="nav-item">
-                        <a class="nav-link" href="#">AboutUs</a>
+                    <li className="nav-item active"><a className="nav-link" href="/">ChooseVise</a></li>
+                    <li style={{float: 'right'}} className="nav-item">
+                        <a className="nav-link" href="#">AboutUs</a>
                     </li>
-                    <li style={{float: 'right'}} class="nav-item">
-                        <a class="nav-link" href="#">Signup/Login</a>
+                    <li style={{float: 'right'}} className="nav-item">
+                        <a className="nav-link" href="#">Signup/Login</a>
                     </li>
-                    <li style={{float: 'right'}} class="nav-item">
-                        <a class="nav-link" href="#">Offers</a>
+                    <li style={{float: 'right'}} className="nav-item">
+                        <a className="nav-link" href="#">Offers</a>
                     </li>
-                    <li style={{float: 'right'}} class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+                    <li style={{float: 'right'}} class="nav-item active"><a className="nav-link" href="#">Home</a></li>
                 </ul>
                 
             </div>
